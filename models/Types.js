7df608efeb5f9812f5c609ea7642963bf61bfabd
dpmurphy11 <dpmurphy_onsite@hotmail.com>
2022-03-29 19:18:55 -0400

    types  model

---
+++ models/Types.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Types extends Model {}
+// lookup of quiz types from api
+
+Types.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    type_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'types',
+  }
+);
+
+module.exports = Types;
