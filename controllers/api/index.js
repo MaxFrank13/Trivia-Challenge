8d87d531521e7eeb2473a18fafaf157ffc3652cb
Max <maxfrank13@gmail.com>
2022-04-04 20:33:47 -0400

    fixed routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const quizRoutes = require('./quizRoutes');
-const scoreRoutes = require('./scoreRoutes');
 const gameRoutes = require('./gameRoutes');
 const formRoutes = require('./formRoutes');
 
 router.use('/users', userRoutes);
-router.use('/quiz', quizRoutes);
 router.use('/game', gameRoutes);
-router.use('/score', scoreRoutes);
 router.use('/form', formRoutes);
 
 module.exports = router;
--- controllers/quizRoutes.js
+++ controllers/quizRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { Quiz, QuizQuestion, QuizAnswers, User, Game, Comment, GameDetail } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Quiz, QuizQuestion, QuizAnswers, User, Game, Comment, GameDetail } = require('../models');
+const withAuth = require('../utils/auth');
 
 // GET the quiz of the day for a given category and difficulty from our database
 router.get('/:category_id/:difficulty_id', withAuth, async (req, res) => {
@@ -25,7 +25,7 @@ router.get('/:category_id/:difficulty_id', withAuth, async (req, res) => {
       where: {
         category_id: req.params.category_id,
         difficulty_id: req.params.difficulty_id,
-        // type_id: req.params.type_id
+
       },
       order: [['date_created', 'DESC']],
     });
@@ -53,12 +53,12 @@ router.get('/:category_id/:difficulty_id', withAuth, async (req, res) => {
 
     // ****
 
-    res.status(200).json(quiz);
+    // res.status(200).json(quiz);
 
-    // res.render('quiz', {
-    //   ...quiz,
-    //   logged_in: req.session.logged_in,
-    // });
+    res.render('quiz', {
+      ...quiz,
+      logged_in: req.session.logged_in,
+    });
 
   } catch (err) {
     res.status(400).json(err);
